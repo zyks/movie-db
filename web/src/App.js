8d82d6c2b36b6774e5b0e0fc62bb0ad45ed8a802
Paweł Zykowski <pazy011@gmail.com>
2019-01-09 22:10:15 +0100

    Add routes for movie browser and movie details subpages

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MovieService from './api/MovieService';
 import MovieBrowser from './components/MovieBrowser/MovieBrowser';
+import MovieDetails from './components/MovieDetails/MovieDetails';
 import './App.css';
 
 
@@ -11,11 +13,21 @@ class App extends Component {
         this.service = new MovieService('ee2d0533');
     }
 
-
     render() {
         return (
             <div className="App">
-                <MovieBrowser service={ this.service } />
+                <BrowserRouter>
+                    <div>
+                        <Route 
+                            exact path="/"  
+                            render={ (props) => <MovieBrowser {...props} service={ this.service } /> }
+                        />
+                        <Route 
+                            path="/:imdbID"
+                            render={ (props) => <MovieDetails {...props} service={ this.service } /> }
+                        />
+                    </div>
+                </BrowserRouter>
             </div>
         );
     }
